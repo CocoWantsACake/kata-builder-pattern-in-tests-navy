5ea53992fa8f027c19879efe75576958262755c1
CocoWantsACake <maxime27130@outlook.fr>
2024-11-21 16:07:15 +0100

    Refactor

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -1,12 +1,5 @@
-import pytest
 from shop import Address
 
+fsf_address = Address("51 Franklin Street", "Fifth Floor", "Boston", "02110", "USA")
+paris_address = Address("33 quai d'Orsay", "", "Paris", "75007", "France")
 
-@pytest.fixture
-def fsf_address():
-    return Address("51 Franklin Street", "Fifth Floor", "Boston", "02110", "USA")
-
-
-@pytest.fixture
-def paris_address():
-    return Address("33 quai d'Orsay", "", "Paris", "75007", "France")
--- python/tests/test_shop.py
+++ python/tests/test_shop.py
@@ -1,11 +1,12 @@
 from shop import Shop, User
+from conftest import fsf_address, paris_address
 
 """
 Added 2 tests in order to test the cases where verified and children OR major 
 Same with unverified
 """
 
-def create_default_user(address, name="bob", email="bob@gmail.com", age=16, verified=True):
+def create_user(address=fsf_address, name="bob", email="bob@gmail.com", age=20, verified=True):
     return User(
         name=name,
         email=email,
@@ -14,32 +15,32 @@ def create_default_user(address, name="bob", email="bob@gmail.com", age=16, veri
         verified=verified
     )
 
-def test_happy_path(fsf_address):
-    user = create_default_user(address=fsf_address, age=25)
+def test_happy_path():
+    user = create_user()
     assert Shop.can_order(user)
     assert not Shop.must_pay_foreign_fee(user)
 
 
-def test_minors_cannot_order_from_the_shop(fsf_address):
-    user = create_default_user(address=fsf_address)
+def test_minors_cannot_order_from_the_shop():
+    user = create_user(age=16)
     assert not Shop.can_order(user)
 
 
-def test_adults_can_order_from_the_shop(fsf_address):
-    user = create_default_user(address=fsf_address, age=20)
+def test_adults_can_order_from_the_shop():
+    user = create_user()
     assert Shop.can_order(user)
 
 
-def test_cannot_order_if_not_verified_child(fsf_address):
-    user = create_default_user(address=fsf_address, verified=False)
+def test_cannot_order_if_not_verified_child():
+    user = create_user(age=16, verified=False)
     assert not Shop.can_order(user)
 
 
-def test_cannot_order_if_not_verified_adult(fsf_address):
-    user = create_default_user(address=fsf_address, age=20, verified=False)
+def test_cannot_order_if_not_verified_adult():
+    user = create_user(verified=False)
     assert not Shop.can_order(user)
 
 
-def test_foreigners_must_be_foreign_fee(paris_address):
-    user = create_default_user(address=paris_address, age=25, verified=False)
+def test_foreigners_must_be_foreign_fee():
+    user = create_user(address=paris_address)
     assert Shop.must_pay_foreign_fee(user)
